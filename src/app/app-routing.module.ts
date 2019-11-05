cf56252d92eff561c4f02ad44394fc17f4681703
jefsantos <jsantos@apoioprodesp.sp.gov.br>
2019-11-05 17:56:36 -0300

    cadastrando vendedor 222

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { UserListComponent } from './pessoa-list/user-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PessoamodelFormComponent } from './pessoamodel-form/pessoamodel-form.component';
-import { PasseiomodelService } from './service/service-passeio/passeiomodel-service.service';
 import { PasseioListComponent } from './passeio-list/passeio-list.component';
+import { PasseiomodelFormComponent } from './passeiomodel-form/passeiomodel-form.component';
  
 const routes: Routes = [
   { path: 'pessoas', component: UserListComponent },
   { path: 'passeios', component: PasseioListComponent },
+  { path: 'addpass', component: PasseiomodelFormComponent},
   { path: 'adduser', component: PessoamodelFormComponent }
 ];
  
